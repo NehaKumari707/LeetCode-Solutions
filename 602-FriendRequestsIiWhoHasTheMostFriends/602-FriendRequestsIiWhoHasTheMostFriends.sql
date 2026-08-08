283dc603aefb0895358d5cd8d541c673afa06c99
Neha Panday <pandayneha6833@gmail.com>
2026-08-08 17:22:08 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,11 @@
+-- Last updated: 8/8/2026, 5:22:08 PM
+# Write your MySQL query statement below
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) AS temp
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
