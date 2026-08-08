b30d96c96a5d1164a838654870e2391bb261f6de
Neha Panday <pandayneha6833@gmail.com>
2026-08-08 17:22:47 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,11 @@
+-- Last updated: 8/8/2026, 5:22:46 PM
+# Write your MySQL query statement below
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev1,
+        LAG(num, 2) OVER (ORDER BY id) AS prev2
+    FROM Logs
+) t
+WHERE num = prev1 AND num = prev2;
