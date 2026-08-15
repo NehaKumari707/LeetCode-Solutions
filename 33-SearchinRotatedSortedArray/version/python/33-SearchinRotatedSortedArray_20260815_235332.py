0e5d87c101e79ac461ba891c65024a233d8245d8
Neha Panday <pandayneha6833@gmail.com>
2026-08-15 23:53:33 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/version/python/33-SearchinRotatedSortedArray_20260815_235332.py
@@ -0,0 +1,28 @@
+# Last updated: 8/15/2026, 11:53:32 PM
+1class Solution(object):
+2    def search(self, nums, target):
+3        left = 0
+4        right = len(nums) - 1
+5
+6        while left <= right:
+7            mid = (left + right) // 2
+8
+9            if nums[mid] == target:
+10                return mid
+11
+12            if nums[left] <= nums[mid]:
+13
+14                if nums[left] <= target < nums[mid]:
+15                    right = mid - 1
+16                else:
+17                    left = mid + 1
+18
+19            else:
+20
+21                if nums[mid] < target <= nums[right]:
+22                    left = mid + 1
+23                else:
+24                    right = mid - 1
+25
+26        return -1
+27        
